e9514654cc7f98a2f7bae9036f5f9eb1e08d7bab
laury <lauryerba@hotmail.fr>
2023-05-20 16:54:39 +0200

    app mise à jour

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
-const express = require("express");
+const path = require('path');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
 const app = express();
 
-const mongoose = require('mongoose');
 
-const stuffRoutes = require('./routes/stuff');
-const path = require('path');
-
-
+// mongoose connect
 mongoose.connect('mongodb+srv://laury:Ramses4269@atlascluster.z4m1ux1.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-  
-  app.use('/api/stuff', stuffRoutes);
-  
-  app.use((req, res, next) => {
+
+// CORS - partage de ressources entre serveurs
+app.use((req, res, next) => 
+{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-
-app.use((req, res) => {
-    res.json({ message: "Votre requête a bien été reçue !" });
 });
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(bodyParser.json()); //bodyparser
+
+app.use(mongoSanitize()); // En prévention des injections
+app.use(helmet()); // helmet
+
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+
+
+app.use('/images', express.static(path.join(__dirname, 'images'))); // gestion images de manière statiques
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
-
-
+    
+ // export de l'application
 module.exports = app;
