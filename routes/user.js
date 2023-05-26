79dff320f1e1999ad6a92525a1a40d46454c1b88
laury <lauryerba@hotmail.fr>
2023-05-26 13:00:51 +0200

    Fin du projet

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-//const rateLimit = require('express-rate-limit'); // package de prévention des forces brutes
 //Controlers
 const userCtrl = require('../controllers/user');
-
-//const passLimiter = rateLimit({
-  //  windowMs: 2 * 60 * 1000, // Temps défini (en minutes) pour tester l'application
-    //max: 3 // essais max par adresse ip
-  //});
 
 //routes POST pour créer un compte ou se connecter envoyé par le frontend
 router.post('/signup', userCtrl.signup);
